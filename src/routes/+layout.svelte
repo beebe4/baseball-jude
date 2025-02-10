3b85a4469aa5e26950f36a5c2a7f7e4bf5b66657
Ben McDonald <ben.mcdonald@corporatetraffic.com>
2025-02-10 15:39:19 -0500

    OG updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <svelte:head>
 	<title>Jude McDonald - Baseball Class of 2026</title>
-	<meta property="og:title" content="Jude McDonald - Baseball Class of 2026" />
+	<meta property="og:title" content="Jude McDonald - RHP - Class of 2026" />
 	<meta property="og:type" content="profile" />
 	<meta property="og:url" content="https://judebaseball.com" />
 	<meta property="og:image" content="https://judebaseball.com/images/jude-pitching.jpeg" />
@@ -10,12 +10,12 @@
 		property="og:description"
 		content="Right-handed pitcher (Class of 2026) at Episcopal School of Jacksonville. 6'3&quot; pitcher combining power, precision, and academic excellence with a 3.9 GPA."
 	/>
-	<meta property="og:site_name" content="Jude McDonald Baseball" />
+	<meta property="og:site_name" content="Jude McDonald - RHP - Class of 2026" />
 
 	<!-- Twitter Card tags -->
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:site" content="@jude_mcdonald7" />
-	<meta name="twitter:title" content="Jude McDonald - Baseball Class of 2026" />
+	<meta name="twitter:title" content="Jude McDonald - RHP - 2026" />
 	<meta
 		name="twitter:description"
 		content="Right-handed pitcher (Class of 2026) at Episcopal School of Jacksonville. 6'3&quot; pitcher combining power, precision, and academic excellence with a 3.9 GPA."
@@ -23,8 +23,8 @@
 	<meta name="twitter:image" content="https://judebaseball.com/images/jude-pitching.jpeg" />
 
 	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+	<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon.svg" />
 	<link rel="manifest" href="/site.webmanifest" />
 	<meta name="theme-color" content="#7bb8b1" />
 </svelte:head>
